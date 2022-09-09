0146a06e19f242c732b1bd30defae0e29eba4902
rajabi <rajabi1380@gmail.com>
2022-09-09 14:41:50 +0430

    KEY

--- keyboard/key.js
+++ keyboard/key.js
@@ -1,9 +1,12 @@
-// const item2 = document.querySelector(".keyboard");
-const i1 = document.querySelectorAll(".item2");
-
-i1.addEventListener("keydown", function (event) {
-  if (event.code == "KeyA" && event.ctrlKey) {
-    console.log("a");
-    // i1.classList.toggle("active");
+const itme2 = document.querySelector(".item2");
+const p = document.querySelector("p");
+itme2.addEventListener("keydown", function (event) {
+  if (event.code == "KeyA") {
+    p.classList.add("active");
+  }
+});
+itme2.addEventListener("keyup", function (event) {
+  if (event.code == "KeyA") {
+    p.classList.remove("active");
   }
 });
